926e1fae009636cbbed7908ad6ac77e5dcef12e5
IADITHYAI <124696065+IADITHYAI@users.noreply.github.com>
2025-12-16 12:28:35 +0530

    Add files via upload

---
+++ Oxy/Oxy/src/main/java/com/Oxy/Oxy/OxyApplication.java
@@ -0,0 +1,13 @@
+package com.Oxy.Oxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxyApplication.class, args);
+	}
+
+}
---
+++ Oxy/Oxy/src/test/java/com/Oxy/Oxy/OxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Oxy.Oxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
